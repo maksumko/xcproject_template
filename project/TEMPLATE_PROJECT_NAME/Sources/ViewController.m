e2e8a8062010d4012b2045fb5c5f9aacf8465412
koshtenko <mkos@ciklum.com>
2014-05-13 19:53:11 +0300

    set organization

--- project/TEMPLATE_PROJECT_NAME/Sources/ViewController.m
+++ project/TEMPLATE_PROJECT_NAME/Sources/ViewController.m
@@ -3,7 +3,7 @@
 //  TEMPLATE_PROJECT_NAME
 //
 //  Created by maxim.k on 5/13/14.
-//  Copyright (c) 2014 TEMPLATE_ORG_NAME. All rights reserved.
+//  Copyright (c) 2014 Ciklum. All rights reserved.
 //
 
 #import "ViewController.h"
